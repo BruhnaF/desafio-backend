8cb7c569acbcaa47c4c6e937be2cf1902ecc0e52
BruhnaF <bruhnaformento@gmail.com>
2025-04-12 15:12:25 -0300

    criação de classe ObjectsValidator onde realiza validação dos dados recebidos

--- desafio-backend/desafio-backend/src/main/java/com/simplesdental/product/service/users/UserService.java
+++ desafio-backend/desafio-backend/src/main/java/com/simplesdental/product/service/users/UserService.java
@@ -1,9 +1,12 @@
 package com.simplesdental.product.service.users;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Service;
 
 import com.simplesdental.product.model.User;
 import com.simplesdental.product.repository.UserRepository;
+import com.simplesdental.product.service.ObjectsValidator;
 import com.simplesdental.product.service.Result;
 
 import lombok.RequiredArgsConstructor;
@@ -12,12 +15,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-
+    private final ObjectsValidator<RegisterRequest> postValidator;
+    
     public Result<RegisterResponse> save(RegisterRequest user) {
+        Result<RegisterResponse> response = new Result<RegisterResponse>();
+        var violations = postValidator.validate(user);
+
+        if (!violations.isEmpty()) {            
+            response.setErrors(new ArrayList<>(violations));
+        }
+
         User newUser = userRepository.save(user.toModel());
         RegisterResponse userResponse = new RegisterResponse(newUser);
-        
-        Result<RegisterResponse> response = new Result<RegisterResponse>();
+
         response.setData(userResponse);
         return response;
     }
