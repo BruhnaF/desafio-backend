87cde708849168ff9856c24ae08cd812f67ecf3d
BruhnaF <bruhnaformento@gmail.com>
2025-04-12 01:08:45 -0300

    Implementação da classe generica para validar se os dados foram salvos corretamente ou falharam

--- desafio-backend/desafio-backend/src/main/java/com/simplesdental/product/controller/UserController.java
+++ desafio-backend/desafio-backend/src/main/java/com/simplesdental/product/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.simplesdental.product.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.simplesdental.product.service.Result;
 import com.simplesdental.product.service.users.RegisterRequest;
 import com.simplesdental.product.service.users.RegisterResponse;
 import com.simplesdental.product.service.users.UserService;
@@ -19,8 +21,13 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/auth/register")
-    public RegisterResponse createUser(@RequestBody RegisterRequest entity) {
-        return userService.save(entity);
+    public ResponseEntity<Object> createUser(@RequestBody RegisterRequest entity) {
+        Result<RegisterResponse> response = userService.save(entity);
+
+        if (!response.isSuccess()) {
+            return ResponseEntity.badRequest().body(response.getErrors());
+        }
+        return ResponseEntity.ok(response.getData());
     }
 
 }
